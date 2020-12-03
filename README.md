1706b6b7d6184094d8d2913259c32aad82b8f25d
monalizards <monalizards@gmail.com>
2020-12-03 21:00:21 +0000

    Add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Happiness Journal
+This project was inspired by a gift given to me by an old friend which was a journal with 365 pages to document things that made me smile every day for a year.
+
+This website is dedicated to those who need reminders of things that made them happy, and things to be grateful for.
+
+Current functionalities of the website are: registration, log in, log out, write, read and remove records.
--- db.sql
+++ db.sql
@@ -15,5 +15,17 @@ CREATE TABLE IF NOT EXISTS 'records' (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-INSERT INTO records ('user_id', 'name')
-    VALUES (1, 'I think ');
+-- seeds
+INSERT INTO users ('username', 'email', 'hash', 'birthday')
+    VALUES ('scchung10', 'chungsumching@gmail.com', '535000$T.qO6MmyAcyTgKvr$xZ060P31YeMjgygAo12paRuz7xrum8GFMT1QN8Ia7uB', '1994-11-06 00:00:00');
+
+INSERT INTO records ('user_id', 'name', 'timestamp')
+    VALUES (1, 'Seeing an old friend', '2020-11-03 15:31:06');
+INSERT INTO records ('user_id', 'name', 'timestamp')
+    VALUES (1, 'Hearing a song that reminds you of your past', '2020-11-03 15:31:06');
+INSERT INTO records ('user_id', 'name', 'timestamp')
+    VALUES (1, 'A bit of good news for a loved one/friend', '2020-11-04 15:31:06');
+INSERT INTO records ('user_id', 'name', 'timestamp')
+    VALUES (1, 'Getting a thank you from someone', '2020-11-04 15:31:06');
+INSERT INTO records ('user_id', 'name', 'timestamp')
+    VALUES (1, 'Eating comfort food', '2020-12-03 15:31:06');
